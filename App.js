5a11d9d1b1dd95f083317a8098a9615d1dae41ea
Melanie Toh <melanietoh@hotmail.com>
2022-04-23 15:13:04 -0700

    Added Zeina's imports

--- App.js
+++ App.js
@@ -3,12 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './Login.js';
 import SignUp from './SignUp'
 import MyStack from './MyStack.js';
+import BlockCard from './app/components/BlockCard';
+import Screen from './app/components/Screen';
+import FeaturedFood from './app/components/FeaturedFood';
+import SearchBar from './app/components/SearchBar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-        <MyStack></MyStack>
-    </View>
+    <Screen>
+        <SearchBar/>
+        <FeaturedFood/>
+        <BlockCard/>
+    </Screen>
   );
 }
 
